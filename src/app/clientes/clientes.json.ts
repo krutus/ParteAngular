48ab9223ae4046f28d64ce7f244f19b9012a178a
Luis Haro <luis.haro@babel.es>
2020-06-09 16:41:15 +0200

    Primera subida, con listados de clientes y cursos

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,12 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    { id: 1, nombre: 'Luis', apellido: 'Haro', email: 'luis.haro@babel.es', createAt: '2020-02-27' },
+    { id: 2, nombre: 'Andrés', apellido: 'Guzmán', email: 'andres.guzman@bolsadeideas.com', createAt: '2020-02-27' },
+    { id: 3, nombre: 'Cliente3', apellido: 'Apellido3', email: 'email3@bolsadeideas.com', createAt: '2020-02-27' },
+    { id: 4, nombre: 'Cliente4', apellido: 'Apellido4', email: 'email4@bolsadeideas.com', createAt: '2020-02-27' },
+    { id: 5, nombre: 'Cliente5', apellido: 'Apellido5', email: 'email5@bolsadeideas.com', createAt: '2020-02-27' },
+    { id: 6, nombre: 'Cliente6', apellido: 'Apellido6', email: 'email6@bolsadeideas.com', createAt: '2020-02-27' },
+    { id: 7, nombre: 'Cliente7', apellido: 'Apellido7', email: 'email7@bolsadeideas.com', createAt: '2020-02-27' },
+    { id: 8, nombre: 'Cliente8', apellido: 'Apellido8', email: 'email8@bolsadeideas.com', createAt: '2020-02-27' }
+  ];
